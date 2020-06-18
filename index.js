093c7dad81ccf1811fc141a5a0b524f63a898fe8
Resha Puspita <reshapuspita734@gmail.com>
2020-06-18 11:02:41 +0700

    add files

---
+++ index.js
@@ -0,0 +1,15 @@
+const buttonCekNomor = document.querySelector(".cek-nomor .buttonCek");
+
+cekNomorHP = (string) => {
+  const regEx = new RegExp(/^\d{10,12}$/);
+  return regEx.test(string);
+};
+// -------------------- EVENT LISTENER ---------------------
+buttonCekNomor.addEventListener("click", () => {
+  const cekNomor = document.getElementById("nomor").value;
+  if (cekNomorHP(cekNomor)) {
+    alert(`${cekNomor} Successfully Added!`);
+  } else {
+    alert("Kurang digitnya atau bisa jadi bukan digit(?)");
+  }
+});
